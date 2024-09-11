a9e8dd9855e38cd3cbd5862aeef061c90eaa82a0
originaluzer <rafalelsynch@gmail.com>
2024-09-11 19:26:22 +0100

    myquizzpro - v1

---
+++ QuizPro-Front/myquizzpro/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Header/>
+     
+
+    </div>
+  );
+}
+
+export default App;
---
+++ QuizPro-Front/myquizzpro/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+  return (
+    <div class="headerContainer">
+    <div class="logoContainer"> Logo </div>
+    <div class="navContainer" >
+      <ul class="ulContainer">
+        <li>How it Works</li>
+        <li>How it Works</li>
+        <li>How it Works</li>
+        <li>How it Works</li>
+        <li><button>Login</button></li>
+        </ul>
+     </div>
+    </div>
+  )
+}
+
+export default Header
